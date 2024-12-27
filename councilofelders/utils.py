c4011c3af1b5a915d32faa8fca87cf02624a7528
rawkintrevo <trevor.d.grant@gmail.com>
2024-12-26 20:51:59 -0600

    Stamp out google ai references

--- councilofelders/utils.py
+++ councilofelders/utils.py
@@ -1,8 +1,5 @@
-from google.ai.generativelanguage_v1beta.types.content import Content as gContent
 def update_role(items, self_name):
     for item in items:
-        if isinstance(item, gContent):
-            item = {'role': item.role, 'parts': item.parts}
         if item['role'] == self_name:
             item['role'] = 'assistant'
         else:
@@ -17,11 +14,7 @@ def merge_items_by_role(items):
     output = [items[0]]
 
     for item in items[1:]:  # Start from the second item
-        if isinstance(item, gContent):
-            item = {'role': item.role, 'parts': item.parts}
         last_item = output[-1]
-        if isinstance(last_item, gContent):
-            last_item = {'role': last_item.role, 'parts': last_item.parts}
         if item['role'] == last_item['role']:
             # If the current item's role matches the last item in the output,
             # append the current content to the last item's content with a newline
